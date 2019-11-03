9c45a165b7dd40ce2912369a0f06eddcce171d2a
Yih Yang <yyngo788n88@hotmail.com>
2019-11-03 16:48:04 +0800

    generated models and migrations

---
+++ database/migrations/2019_11_03_075137_create_workshops_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name');
+            $table->string('contact_number');
+            $table->double('latitude');
+            $table->double('longitude');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
